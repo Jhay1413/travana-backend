de070f37c64f63a416f78d967a14aeeecbbbc402
Jhay1413 <jhon041413@gmail.com>
2025-10-22 17:53:18 +0800

    fix json upload

--- src/repository/quote.repo.ts
+++ src/repository/quote.repo.ts
@@ -632,7 +632,7 @@ export const quoteRepo: QuoteRepo = {
         if (data.passengers && data.passengers.length > 0) {
           await tx.insert(passengers).values(
             data.passengers.map((data) => ({
-              type: data.type,
+              type: data.type ?? "adults",
               age: data.age,
               quote_id: quote_id.id,
             }))
